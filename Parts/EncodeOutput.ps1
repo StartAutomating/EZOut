82828ea7aa9671b02ea4868412c28d3c5396a4fd
StartAutomating <james.brundage@start-automating.com>
2020-02-23 18:47:28 -0800

    Initial Formatting Parts

---
+++ Parts/EncodeOutput.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+    Encodes output (if required)
+.Description
+    Encodes output, if required by the host.
+
+    If a $request variable is present, or $host.UI.SupportsHTML, each argument will be HTML encoded.
+    Otherwise, each argument will be passed thru.
+.Notes
+    IsFormatPart: true
+#>
+param()
+if ($request -or $Host.UI.SupportsHTML) 
+{
+    foreach ($a in $args) {
+        [Net.WebUtility]::HtmlEncode("$a")
+    }
+}
+else 
+{
+    foreach ($a in $args) {
+        $a
+    }
+}
