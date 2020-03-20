f821d4d9ffa446f67ddfcc571046524174b1bfcb
StartAutomating <james.brundage@start-automating.com>
2020-03-19 20:20:38 -0700

    Using margin-left ex instead of margin-left em (thanks @vexx32)

--- Parts/Indent.ps1
+++ Parts/Indent.ps1
@@ -6,7 +6,7 @@
 
     In most scenarios, content is indented by a number of spaces
 
-    If $request or $host.UI.SupportsHTML, this will render a div tag with margin-left set to $Length em 
+    If $request or $host.UI.SupportsHTML, this will render a div tag with margin-left set to $Length ex 
     (this is the equivilent of N characters of indentation in HTML)
 .Notes
     IsFormatPart: true
@@ -21,7 +21,7 @@ $Length = 4
 )
 
 if ($Request -or $Host.UI.SupportsHTML) {
-    return "<div style='margin-left:${Length}em'>$content</div>"
+    return "<div style='margin-left:${Length}ex'>$content</div>"
 }
 
 (' ' * $Length) + "$content"
