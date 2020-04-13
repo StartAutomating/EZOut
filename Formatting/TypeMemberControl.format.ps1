04851552b81f688b36e2a7681ae5fa410da78a92
StartAutomating <james.brundage@start-automating.com>
2020-04-13 13:17:36 -0700

    Adding Controls for Fields and Members

---
+++ Formatting/TypeMemberControl.format.ps1
@@ -0,0 +1,12 @@
+﻿Write-FormatControl -Name TypeMemberControl -Action {
+    Write-FormatViewExpression -If { 
+        'RuntimeMethodInfo', 'RuntimeConstructorInfo' -contains $_.GetType().Name
+    } -ScriptBlock  {$_ } -ControlName TypeMethodControl
+    Write-FormatViewExpression -If { $_.GetType().Name -eq 'RuntimePropertyInfo' } -ScriptBlock  {$_ } -ControlName TypePropertyControl
+    Write-FormatViewExpression -If { 
+        
+        'MdFieldInfo', 'RtFieldInfo' -contains $_.GetType().Name 
+    } -ScriptBlock  {$_ } -ControlName TypeFieldControl
+    Write-FormatViewExpression -If { $_.GetType().Name -eq 'RuntimeEventInfo' } -ScriptBlock  {$_ } -ControlName TypeEventControl
+}
+
