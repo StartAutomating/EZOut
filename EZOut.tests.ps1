f7f4e30e58ae05817a552c0840a5d41d4c0e1efc
StartAutomating <james.brundage@start-automating.com>
2022-04-19 01:36:21 -0700

    Updating test for Format-Object (adding tests for -MarkdownTable)

--- EZOut.tests.ps1
+++ EZOut.tests.ps1
@@ -1154,5 +1154,6 @@ describe 'Format-Object' {
             "$('red' | Format-Object -ForegroundColor "Red")" | Should -Match '\e.+Red'
         }
         "1","2","3" | Format-Object -YamlHeader | Should -BeLike '*- 1*- 2*- 3*'
+        [PSCustomObject]@{a='b';c='d'} | Format-Object -MarkdownTable | Should -match '\|a\|c\|[-\|]+\|b\|d\|'
     }
 }
