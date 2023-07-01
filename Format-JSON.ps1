17333aa7bd7e5799935c6884f0b21f7cf39297e0
StartAutomating <StartAutomating@github.com>
2023-07-01 21:40:43 +0000

    Write-FormatViewExpression - Adding -Style (Fixes #106)

--- Format-JSON.ps1
+++ Format-JSON.ps1
@@ -1,30 +1,30 @@
 function Format-JSON {
-<#
-    .SYNOPSIS
-        Formats objects as JSON
-    .DESCRIPTION
-        Formats an object as JSON.
-        This is a light wrapper around ConvertTo-Json with a few key differences:
-        1. It defaults -Depth to 100 (the maximum)
-        2. It will not encode strings that look like JSON.  Format-JSON can have raw JSON as input without it being converted.
-        3. It allows you to force single values into a list with -AsList
-        4. If there is nothing to convert, it outputs an empty JSON object.
-        Using Format-JSON inside of an EZOut command will pack Format-JSON into your exported .ps1xml.
-    .EXAMPLE
-        Format-JSON -InputObject @("a", "b", "c")
-    .EXAMPLE
-        [Ordered]@{a="b";c="d";e=[Ordered]@{f=@('g')}} | Format-JSON
+    <#
     
-#>
+    .SYNOPSIS    
+        Formats objects as JSON    
+    .DESCRIPTION    
+        Formats an object as JSON.    
+        This is a light wrapper around ConvertTo-Json with a few key differences:    
+        1. It defaults -Depth to 100 (the maximum)    
+        2. It will not encode strings that look like JSON.  Format-JSON can have raw JSON as input without it being converted.    
+        3. It allows you to force single values into a list with -AsList    
+        4. If there is nothing to convert, it outputs an empty JSON object.    
+        Using Format-JSON inside of an EZOut command will pack Format-JSON into your exported .ps1xml.    
+    .EXAMPLE    
+        Format-JSON -InputObject @("a", "b", "c")    
+    .EXAMPLE    
+        [Ordered]@{a="b";c="d";e=[Ordered]@{f=@('g')}} | Format-JSON    
     
+    #>
+            
     [Management.Automation.Cmdlet("Format", "Object")]
-[CmdletBinding(HelpUri='https://go.microsoft.com/fwlink/?LinkID=2096925', RemotingCapability='None')]
     param(
-# If set, will always Format-JSON as a list.
+    # If set, will always Format-JSON as a list.    
     [switch]
     $AsList
     )
-dynamicParam {
+    dynamicParam {
     $baseCommand = 
         if (-not $script:ConvertToJSON) {
             $script:ConvertToJSON = 
@@ -57,20 +57,20 @@ dynamicParam {
         ))
     }
     $DynamicParameters
-}
-    begin {
+    }
+        begin {
         $accumulateInput = [Collections.Queue]::new()
         $rawJSON         = [Collections.Queue]::new()
     
-}
-    process {
+    }
+        process {
         if ($_ -is [string] -and $_ -match '^\s{0,}[\[\{"]' -and $_ -match '[\]\}"]\s{0,}$') {
             $rawJSON.Enqueue($_)
         } else {
             $accumulateInput.Enqueue($_)        
         }        
     
-}
+    }
     end {
         $joiner = "," + $(
             if (-not $PSBoundParameters["Compress"]) {
