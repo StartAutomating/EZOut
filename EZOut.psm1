9c9629644a2fd3375a37a3bfc1d9c70b04ec8228
StartAutomating <james.brundage@start-automating.com>
2023-07-11 17:40:29 -0700

    Renaming Add-TypeData to Push-TypeData (Fixes #113)
    Also, beginning repository refactor (re #115, #117)

--- EZOut.psm1
+++ EZOut.psm1
@@ -4,8 +4,20 @@ param()
 $Script:FormatModules = @{}
 $script:TypeModules = @{}
 
+$commandsPath = Join-Path $psScriptRoot "Commands"
+
+:ToIncludeFiles foreach ($file in (Get-ChildItem -Path "$commandsPath" -Filter "*-*" -Recurse)) {
+    if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
+    foreach ($exclusion in '\.[^\.]+\.ps1$') {
+        if (-not $exclusion) { continue }
+        if ($file.Name -match $exclusion) {
+            continue ToIncludeFiles  # Skip excluded files
+        }
+    }
+    . $file.FullName
+}
+
 #region Formatters
-. $psScriptRoot\Add-FormatData.ps1
 . $psScriptRoot\Clear-FormatData.ps1
 . $psScriptRoot\Remove-FormatData.ps1
 
