151f7634afe2ac3157c3bf20164f7b8779fb7541
StartAutomating <james.brundage@start-automating.com>
2023-07-12 12:37:39 -0700

    Moving Action/Workflow into Build
    Fixes #116, re #115

---
+++ Build/EZOut.GitHubWorkflow.PSDevOps.ps1
@@ -0,0 +1,16 @@
+﻿#requires -Module EZOut
+#requires -Module PSDevOps
+Import-BuildStep -ModuleName EZOut
+Push-Location ($PSScriptRoot | Split-Path)
+New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -On Demand, Push -Job PowerShellStaticAnalysis, 
+    TestPowerShellOnLinux,
+    TagReleaseAndPublish,
+    BuildEZOut -OutputPath (
+        Join-Path $pwd .\.github\workflows\TestReleaseAndPublish.yml
+    )    
+
+New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged -OutputPath (
+    Join-Path $pwd .github\workflows\OnIssue.yml
+)
+
+Pop-Location
