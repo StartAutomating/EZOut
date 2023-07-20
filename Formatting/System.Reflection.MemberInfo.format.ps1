c5bef3f697f00e266cf370b5397dbe2443914ddf
StartAutomating <james.brundage@start-automating.com>
2023-07-19 22:32:41 -0700

    Moving Example Formatters to new module (Fixes #127)

--- Formatting/System.Reflection.MemberInfo.format.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-FormatView -TypeName System.Reflection.MemberInfo -Action {
-    Write-FormatViewExpression -If { $ExecutionContext.SessionState.PSVariable.Set('Script:DisplayingMember',$true) } -ScriptBlock { $null } 
-    Write-FormatViewExpression -ControlName TypeMemberControl -ScriptBlock { $_ } 
-    Write-FormatViewExpression -If { $ExecutionContext.SessionState.PSVariable.Remove('Script:DisplayingMember') } -ScriptBlock { $null } 
-}
--- Formatting/TypeBaseControl.format.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-FormatView -Action {
-    Write-FormatViewExpression -Newline
-    Write-FormatViewExpression -ScriptBlock { '  *' }
-    Write-FormatViewExpression -ControlName TypeNameControl -ScriptBlock  {$_ } -ForegroundColor 'Verbose'
-} -TypeName TypeBase -Name TypeBase -AsControl
